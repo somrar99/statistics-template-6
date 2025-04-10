1f6ab9c43baa8c134254b2888532ae2a2d9acc10
Kun Song <somrar99@gmail.com>
2025-04-10 21:39:58 +0200

    init repo
    ignore .DS_Store

---
+++ js/_menu.js
@@ -0,0 +1,6 @@
+createMenu('Statistics Template JS', [
+  { name: 'Nytt i version 6', script: 'new-in-v6.js' },
+  { name: 'Visa ett år', script: 'one-year.js' },
+  { name: 'Jämför två år', script: 'compare-two-years.js' },
+  { name: 'Hitta trender', script: 'trends.js' }
+]);
