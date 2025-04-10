db5187ab63dbb123393e71d1d57382ce2e1ec56f
Kun Song <somrar99@gmail.com>
2025-04-10 22:04:47 +0200

    add db

--- js/new-in-v6.js
+++ js/new-in-v6.js
@@ -1,4 +1,11 @@
+
+
 addMdToPage(`
   ### Det finns mycket nytt och förbättrat i version 6!
   Läs om allt nytt och gammalt [i den inbyggda dokumentationen](/docs)!
 `);
+
+let students = await dbQuery('SELECT * FROM result LIMIT 5')
+
+console.log(students)
+tableFromData({ data: students });
